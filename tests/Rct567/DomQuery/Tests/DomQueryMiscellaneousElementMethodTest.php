34e09a1ead3b986efe71354eb943e13ef9f32382
Rick <rct999@gmail.com>
2023-09-14 01:46:33 +0200

    create seperate test file for 'misc > elm methods'

---
+++ tests/Rct567/DomQuery/Tests/DomQueryMiscellaneousElementMethodTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Rct567\DomQuery\Tests;
+
+use Rct567\DomQuery\DomQuery;
+
+class DomQueryMiscellaneousElementMethodTest extends \PHPUnit\Framework\TestCase
+{
+     /*
+     * Test get data
+     */
+    public function testGetData()
+    {
+        $dom = new DomQuery('<div data-role="page"></div>');
+        $this->assertEquals('page', $dom->data('role'));
+        $this->assertNull($dom->data('nope'));
+        $this->assertEquals((object) array('role' => 'page'), $dom->data());
+    }
+
+    /*
+     * Test get object data
+     */
+    public function testGetObjectData()
+    {
+        $dom = new DomQuery('<div data-options=\'{"name":"John"}\'></div>');
+        $this->assertEquals((object) array('name' => 'John'), $dom->data('options'));
+        $this->assertEquals((object) array('options' => (object) array('name' => 'John')), $dom->data());
+    }
+
+    /*
+     * Test set string as data, surviving wrap
+     */
+    public function testSetStringData()
+    {
+        $dom = new DomQuery('<div> <a data-role=""></a> </div>');
+        $dom->find('a')->data('role', 'page');
+        $this->assertEquals('page', $dom->find('a')->data('role'));
+        $dom->find('a')->wrap('<div>');
+        $this->assertEquals('page', $dom->find('a')->data('role'));
+        $this->assertEquals((object) array('role' => 'page'), $dom->find('a')->data());
+    }
+
+    /*
+     * Test set instance as data, surviving wrap
+     */
+    public function testSetInstanceData()
+    {
+        $dom = new DomQuery('<div> <a data-role=""></a> </div>');
+        $dom->find('a')->data('role', $this);
+        $this->assertEquals($this, $dom->find('a')->data('role'));
+        $dom->find('a')->wrap('<div>');
+        $this->assertEquals($this, $dom->find('a')->data('role'));
+    }
+
+    /*
+     * Test remove data
+     */
+    public function testRemoveData()
+    {
+        $dom = new DomQuery('<div> <a></a> </div>');
+        $dom->find('a')->data('role', 'page');
+        $dom->find('a')->data('other', 'still-there');
+        $this->assertEquals('page', $dom->find('a')->data('role'));
+        $this->assertEquals('still-there', $dom->find('a')->data('other'));
+        $dom->find('a')->removeData('role');
+        $this->assertEquals(null, $dom->find('a')->data('role'));
+        $this->assertEquals('still-there', $dom->find('a')->data('other'));
+    }
+
+    /*
+     * Test remove all data
+     */
+    public function testRemoveAllData()
+    {
+        $dom = new DomQuery('<div> <a data-stay="x"></a> </div>');
+        $dom->find('a')->data('role', 'page');
+        $dom->find('a')->data('other', 'also-gone');
+        $this->assertEquals('page', $dom->find('a')->data('role'));
+        $dom->find('a')->removeData();
+        $this->assertEquals(null, $dom->find('a')->data('role'));
+        $this->assertEquals(null, $dom->find('a')->data('other'));
+        $this->assertEquals('<div> <a data-stay="x"></a> </div>', (string) $dom);
+    }
+
+    /*
+     * Test get index of position in dom
+     */
+    public function testIndex()
+    {
+        $dom = new DomQuery('<div> <a class="a">1</a><a class="b">2</a><a class="c">3</a><a class="d">4</a> </div>');
+        $this->assertEquals(-1, $dom->find('nope')->index());
+        $this->assertEquals(0, $dom->find('div')->index());
+        $this->assertEquals(0, $dom->find('.a')->index());
+        $this->assertEquals(3, $dom->find('.d')->index());
+    }
+
+    /*
+     * Test get index of position in result that matches selector
+     */
+    public function testIndexWithSelector()
+    {
+        $dom = new DomQuery('<div> <a class="a">1</a><a class="b">2</a><a class="c">3</a><a class="d">4</a> </div>');
+        $this->assertEquals(-1, $dom->find('a')->index('.nope'));
+        $this->assertEquals(0, $dom->find('a')->index('.a'));
+        $this->assertEquals(1, $dom->find('a')->index($dom->find('.b')));
+        $this->assertEquals(2, $dom->find('a')->index($dom->find('a')->get(2)));
+        $this->assertEquals(3, $dom->find('a')->index('.d'));
+    }
+
+    /*
+     * Test each iteration
+     */
+    public function testEachIteration()
+    {
+        $dom = new DomQuery('<p> <a>1</a> <a>2</a> <span></span> </p>');
+
+        $result = array();
+
+        $dom->find('a')->each(function ($elm, $i) use (&$result) {
+            $result[$i] = $elm;
+        });
+
+        $this->assertCount(2, $result);
+        $this->assertInstanceOf(\DOMNode::class, $result[0]);
+    }
+}
--- tests/Rct567/DomQuery/Tests/DomQueryTest.php
+++ tests/Rct567/DomQuery/Tests/DomQueryTest.php
@@ -121,106 +121,7 @@ class DomQueryTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('<div selected>a</div>', (string) $dom);
     }
 
-    /*
-     * Test get data
-     */
-    public function testGetData()
-    {
-        $dom = new DomQuery('<div data-role="page"></div>');
-        $this->assertEquals('page', $dom->data('role'));
-        $this->assertNull($dom->data('nope'));
-        $this->assertEquals((object) array('role' => 'page'), $dom->data());
-    }
 
-    /*
-     * Test get object data
-     */
-    public function testGetObjectData()
-    {
-        $dom = new DomQuery('<div data-options=\'{"name":"John"}\'></div>');
-        $this->assertEquals((object) array('name' => 'John'), $dom->data('options'));
-        $this->assertEquals((object) array('options' => (object) array('name' => 'John')), $dom->data());
-    }
-
-    /*
-     * Test set string as data, surviving wrap
-     */
-    public function testSetStringData()
-    {
-        $dom = new DomQuery('<div> <a data-role=""></a> </div>');
-        $dom->find('a')->data('role', 'page');
-        $this->assertEquals('page', $dom->find('a')->data('role'));
-        $dom->find('a')->wrap('<div>');
-        $this->assertEquals('page', $dom->find('a')->data('role'));
-        $this->assertEquals((object) array('role' => 'page'), $dom->find('a')->data());
-    }
-
-    /*
-     * Test set instance as data, surviving wrap
-     */
-    public function testSetInstanceData()
-    {
-        $dom = new DomQuery('<div> <a data-role=""></a> </div>');
-        $dom->find('a')->data('role', $this);
-        $this->assertEquals($this, $dom->find('a')->data('role'));
-        $dom->find('a')->wrap('<div>');
-        $this->assertEquals($this, $dom->find('a')->data('role'));
-    }
-
-    /*
-     * Test remove data
-     */
-    public function testRemoveData()
-    {
-        $dom = new DomQuery('<div> <a></a> </div>');
-        $dom->find('a')->data('role', 'page');
-        $dom->find('a')->data('other', 'still-there');
-        $this->assertEquals('page', $dom->find('a')->data('role'));
-        $this->assertEquals('still-there', $dom->find('a')->data('other'));
-        $dom->find('a')->removeData('role');
-        $this->assertEquals(null, $dom->find('a')->data('role'));
-        $this->assertEquals('still-there', $dom->find('a')->data('other'));
-    }
-
-    /*
-     * Test remove all data
-     */
-    public function testRemoveAllData()
-    {
-        $dom = new DomQuery('<div> <a data-stay="x"></a> </div>');
-        $dom->find('a')->data('role', 'page');
-        $dom->find('a')->data('other', 'also-gone');
-        $this->assertEquals('page', $dom->find('a')->data('role'));
-        $dom->find('a')->removeData();
-        $this->assertEquals(null, $dom->find('a')->data('role'));
-        $this->assertEquals(null, $dom->find('a')->data('other'));
-        $this->assertEquals('<div> <a data-stay="x"></a> </div>', (string) $dom);
-    }
-
-    /*
-     * Test get index of position in result that matches selector
-     */
-    public function testIndexWithSelector()
-    {
-        $dom = new DomQuery('<div> <a class="a">1</a><a class="b">2</a><a class="c">3</a><a class="d">4</a> </div>');
-        $this->assertEquals(-1, $dom->find('a')->index('.nope'));
-        $this->assertEquals(0, $dom->find('a')->index('.a'));
-        $this->assertEquals(1, $dom->find('a')->index($dom->find('.b')));
-        $this->assertEquals(2, $dom->find('a')->index($dom->find('a')->get(2)));
-        $this->assertEquals(3, $dom->find('a')->index('.d'));
-    }
-
-    /*
-     * Test get index of position in dom
-     */
-    public function testIndex()
-    {
-        $dom = new DomQuery('<div> <a class="a">1</a><a class="b">2</a><a class="c">3</a><a class="d">4</a> </div>');
-        $this->assertEquals(-1, $dom->find('nope')->index());
-        $this->assertEquals(0, $dom->find('div')->index());
-        $this->assertEquals(0, $dom->find('.a')->index());
-        $this->assertEquals(3, $dom->find('.d')->index());
-    }
 
     /*
      * Test change attribute without value in xml write mode
@@ -239,23 +140,6 @@ class DomQueryTest extends \PHPUnit\Framework\TestCase
     {
         $dom = new DomQuery("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n\n<div selected>a</div>");
         $this->assertEquals("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n\n<div>a</div>", (string) $dom);
-    }
-
-    /*
-     * Test each iteration
-     */
-    public function testEachIteration()
-    {
-        $dom = new DomQuery('<p> <a>1</a> <a>2</a> <span></span> </p>');
-
-        $result = array();
-
-        $dom->find('a')->each(function ($elm, $i) use (&$result) {
-            $result[$i] = $elm;
-        });
-
-        $this->assertCount(2, $result);
-        $this->assertInstanceOf(\DOMNode::class, $result[0]);
     }
 
     /*
@@ -316,8 +200,7 @@ class DomQueryTest extends \PHPUnit\Framework\TestCase
         $nodes_array = DomQuery::create('<div>X</div><p>Nope</p>')->toArray();
         $this->assertContainsOnlyInstancesOf(\DOMNode::class, $nodes_array);
         $this->assertCount(2, $nodes_array);
-        $dom = new DomQuery($nodes_array);
-        $this->assertEquals('<div>X</div><p>Nope</p>', (string) $dom);
+        $this->assertEquals('<div>X</div><p>Nope</p>', (string) DomQuery::create($nodes_array));
     }
 
     /*
